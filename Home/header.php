87919490c935885a9aff07716682124dcf14de20
ArslBaba <arslankhanhaq332@gmail.com>
2022-02-21 20:23:09 +0500

    Alpha Squad

---
+++ Home/header.php
@@ -0,0 +1,32 @@
+<!-- <link rel="stylesheet" href="header.css">
+<header>
+    <nav class="navbar_contents">
+        <div class="logo">
+            <img src="../Home/Vector.png" alt="vector.png">
+            
+            <span class="overview">
+                Overview
+            </span>
+            <span class="pricing">
+                Pricing
+            </span>
+            
+            <span class="case">
+                Case Studies
+            </span>
+            
+            <span class="faq">
+                FAQ
+            </span>
+            <span class="signin">
+                <button class="insideb1">Sign IN</button>
+            </span>
+            <br>
+            <span class="logoname">
+                <img src="../Home/WEBBER.png" alt="Webber">
+            </span>
+        </div>
+        
+    </nav>
+</header> -->
+echo "hello"
---
+++ Readme.md
@@ -0,0 +1,6 @@
+1) Use Basic HTML and CSS3 not any framework
+2) For Server:
+    Use VScode Live Server Extension
+3) All Used Pictures are in Home Folder
+4) Every page have a different folder as per name
+5) Website is not responsive yet
